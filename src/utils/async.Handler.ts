fb17eb09f1f4edb682fd082414b58c49fc17cacb
Zenon <abhishekyadav276208@gmail.com>
2025-11-27 04:35:07 +0530

    error response async handlers done

--- src/utils/async.Handler.ts
+++ src/utils/async.Handler.ts
@@ -1,1 +1,18 @@
-const asyncHandler = async 
+import { NextFunction, Request, Response } from "express";
+
+export const asyncHandler = (requestHandler: Function) => {
+    return (req:Request, res: Response, next: NextFunction) => {
+        Promise.resolve(
+            requestHandler(req, res, next))
+            .catch((err) => next(err));
+    }
+}
+
+
+// export const asyncHandler = (requestHandler: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+//     return (req: Request, res: Response, next: NextFunction) => {
+//         Promise.resolve(
+//             requestHandler(req, res, next))
+//             .catch(next);
+//     }
+// };
