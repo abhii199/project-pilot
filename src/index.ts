b69ea7b06f709b9095457c2fa475b7578dcfd890
Zenon <abhishekyadav276208@gmail.com>
2025-11-27 03:31:22 +0530

    all utility setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("first")
+import dotenv from 'dotenv';
+import app from './app';
+import connectDB from './db/dbConnect';
+
+dotenv.config({
+    path: "./.env"
+})
+
+const PORT = process.env.PORT ?? 8000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error", err);
+        process.exit(1);
+    })
+ 
